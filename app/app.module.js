b5dc00052d63bf7d8ee0efb403b8cb58e94e6ec0
Bruce McElroy <bruce.mcelroy@gmail.com>
2016-01-07 16:14:23 -0500

    set up base app

---
+++ app/app.module.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+
+    angular.module('app', [
+        /* common modules */
+        'app.core',
+
+        /* feature modules */
+        'app.boards'
+    ]);
+})();
---
+++ app/core/core.module.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    angular.module('core', [
+        // angular modules
+        'ngRoute'
+        // app code modules
+        // 3rd party modules
+    ]);
+})();

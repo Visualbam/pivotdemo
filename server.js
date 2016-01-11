d29cf0aff8fd8526c32cc1f8b72db72f48796973
Bruce McElroy <bruce.mcelroy@gmail.com>
2016-01-11 03:39:35 -0500

    organized project

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Route home page to client index file
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use(express.static(__dirname + '/client'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+app.listen('3000', function () {
+    console.log('Im listening...');
+});

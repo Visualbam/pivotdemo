3c639fe6a56db41ef051556d6cc2e0baaabb0192
Bruce McElroy <bruce.mcelroy@gmail.com>
2016-01-11 01:24:16 -0500

    display stories in correct board

--- app/boards/BoardsController.js
+++ app/boards/BoardsController.js
@@ -9,18 +9,47 @@
 
     function BoardsController(pivotTrakerService) {
         var vm = this,
-            data;
+            data,
+            createBoard,
+            backlog,
+            working,
+            done;
 
         // interface
         vm.title = '';
-        vm.stories = [];
+        vm.boards = [];
+
+        createBoard = function (name) {
+            return {
+                title: name,
+                stories: []
+            };
+        };
+
+        backlog = createBoard('backlog');
+        working = createBoard('working');
+        done = createBoard('done');
+
+        vm.boards.push(done, working, backlog);
 
         pivotTrakerService.profile.get(function (data) {
             vm.title = data.email;
         });
 
         pivotTrakerService.stories.query(function (data) {
-            vm.stories = data;
+            angular.forEach(data, function (story) {
+                if (story.current_state === 'unstarted') {
+                    backlog.stories.push(story);
+                }
+
+                if (story.current_state === 'started' || story.current_state === 'finished' || story.current_state === 'delivered') {
+                    working.stories.push(story);
+                }
+
+                if (story.current_state === 'accepted') {
+                    done.stories.push(story);
+                }
+            });
         });
     }
 })();

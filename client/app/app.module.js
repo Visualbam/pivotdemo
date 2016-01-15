b5a6cdf98d079eab951a6ac82161e5a49ab22aba
Bruce McElroy <bruce.mcelroy@Gmail.com>
2016-01-14 21:56:17 -0500

    more file clean up and added sorting / dragging

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app', ['ngRoute', 'ngResource'])
+        .module('app', ['ngRoute', 'ngResource', 'dndLists'])
         .config(routeConfig);
 
     routeConfig.$inject = ['$routeProvider'];
--- client/app/boards/BoardsController.js
+++ client/app/boards/BoardsController.js
@@ -8,26 +8,22 @@
     BoardsController.$inject = ['pivotTrakerService'];
 
     function BoardsController(pivotTrakerService) {
-        var vm = this,
-            backlog,
-            working,
-            done,
-            Board;
+        var vm = this;;
 
         // interface
         vm.title = '';
-        vm.boards = [];
-
-        Board = function (name) {
-            this.title = name;
-            this.stories = [];
+        vm.models = {
+            selected: null,
+            boards: {
+                working: [],
+                done: [],
+                backlog: []
+            }
         };
 
-        backlog = new Board('backlog');
-        working = new Board('working');
-        done = new Board('done');
-
-        vm.boards.push(done, working, backlog);
+        vm.sortStory = function () { 
+            console.log('hello tits');
+        }
 
         pivotTrakerService.profile.get(function (data) {
             vm.title = data.email;
@@ -36,17 +32,19 @@
         pivotTrakerService.stories.query(function (data) {
             angular.forEach(data, function (story) {
                 if (story.current_state === 'unstarted') {
-                    backlog.stories.push(story);
+                    vm.models.boards.backlog.push(story);
                 }
 
                 if (story.current_state === 'started' || story.current_state === 'finished' || story.current_state === 'delivered') {
-                    working.stories.push(story);
+                    vm.models.boards.working.push(story);
                 }
 
                 if (story.current_state === 'accepted') {
-                    done.stories.push(story);
+                    vm.models.boards.done.push(story);
                 }
             });
         });
+        
+        return vm;
     }
 })();

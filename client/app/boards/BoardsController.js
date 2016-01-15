246aaefe49b405edb5853eafdd0ab448ed8644f1
Bruce McElroy <bruce.mcelroy@Gmail.com>
2016-01-15 11:02:29 -0500

    stopping point

--- client/app/boards/BoardsController.js
+++ client/app/boards/BoardsController.js
@@ -8,34 +8,58 @@
     BoardsController.$inject = ['pivotTrakerService', '$scope'];
 
     function BoardsController(pivotTrakerService, $scope) {
+        var storyId;
+
+        $scope.backlog = [];
+        $scope.done = [];
+        $scope.working = [];
+
         $scope.models = {
             selected: null,
             boards: {
-                backlog: [],
-                working: [],
-                done: []
+                backlog: $scope.backlog,
+                working: $scope.working,
+                done: $scope.done
             }
         };
 
-        $scope.updateStatus = function (storyId, event) {
-            var story = pivotTrakerService.story.get({ id: storyId });
-            console.log(event);
+        $scope.getStoryId = function (id) {
+            storyId = id;
         };
+        
+        // $scope.updateStatus = function () {
+        //     var story = pivotTrakerService.story.get({ id: storyId });
+        //     // save request
+        // };
+        
+        $scope.dropCallback = function(event, index, item, external, type, allowedType) {
+            console.log(index);
+            if (external) {
+                if (allowedType === 'itemType' && !item.label) return false;
+                if (allowedType === 'containerType' && !angular.isArray(item)) return false;
+            }
+            return item;
+        };
+       
+        $scope.$watchCollection('backlog', function(one, two, scope) {
+            console.log(one.length)
+        }, true);
 
         pivotTrakerService.stories.query(function (data) {
             angular.forEach(data, function (story) {
                 if (story.current_state === 'unstarted') {
-                    $scope.models.boards.backlog.push(story);
+                    $scope.backlog.push(story);
                 }
 
                 if (story.current_state === 'started' || story.current_state === 'finished' || story.current_state === 'delivered') {
-                    $scope.models.boards.working.push(story);
+                    $scope.working.push(story);
                 }
 
                 if (story.current_state === 'accepted') {
-                    $scope.models.boards.done.push(story);
+                    $scope.done.push(story);
                 }
             });
         });
+
     }
 })();

ac785ae345813c94d80a1c5d450ef0e6b6722b90
Bruce McElroy <bruce.mcelroy@Gmail.com>
2016-01-16 22:27:15 -0500

    renamed main controller

--- client/app/stories/StoriesController.js
+++ client/app/stories/StoriesController.js
@@ -5,11 +5,20 @@
         .module('app')
         .controller('StoriesController', StoriesController);
 
-    StoriesController.$inject = ['pivotTrakerService', '$scope'];
+    StoriesController.$inject = ['pivotTrakerService', '$scope', 'Board'];
 
-    function StoriesController(pivotTrakerService, $scope) {
+    function StoriesController(pivotTrakerService, $scope, Board) {
+        // properties
+        $scope.boards = [];
         $scope.stories = [];
 
+        // build boards
+        $scope.backlog = Board.build('Backlog', ['unstarted']);
+        $scope.working = Board.build('Working', ['started', 'finished', 'delivered', 'rejected']);
+        $scope.done = Board.build('Done', ['accepted']);
+
+        // get stories and sort board they belong to
+        // TODO: come up with a better way to filter using allowedTypes
         pivotTrakerService.stories.query(function (stories) {
             angular.forEach(stories, function (data) {
                 $scope.story = {
@@ -18,33 +27,42 @@
                     current_state: data.current_state
                 }
 
-                $scope.stories.push($scope.story);
+                if (data.current_state === 'unstarted') {
+                    $scope.backlog.list.push($scope.story);
+                }
+
+                else if (data.current_state === 'started' || data.current_state === 'finished' || data.current_state === 'delivered' || data.current_state === 'rejected') {
+                    $scope.working.list.push($scope.story);
+                }
+
+                else if (data.current_state === 'accepted') {
+                    $scope.done.list.push($scope.story);
+                }
             });
         });
 
-        $scope.removeStoryNode = function (event, storyId) {
-            var element = event.srcElement;
-            element.parentNode.removeChild(element);
-        };
+        // create boards array with generated boards
+        $scope.boards.push($scope.backlog, $scope.working, $scope.done);
 
         $scope.updateStory = function (event, index, item) {
-            var board = event.path[2].parentElement.className;
+            var board = event.path[2].className;
 
             if (board.indexOf('Backlog') !== -1) {
                 item.current_state = 'unstarted';
+                $scope.backlog.list.push(item);
             }
 
             if (board.indexOf('Working') !== -1) {
                 item.current_state = 'started';
+                $scope.working.list.push(item);
             }
 
             if (board.indexOf('Done') !== -1) {
                 item.current_state = 'accepted';
+                $scope.done.list.push(item);
             }
 
             pivotTrakerService.story.update({ id: item.id }, item);
-            $scope.stories.push(item);
         };
-
     }
 })();
